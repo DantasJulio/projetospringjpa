adc4355009df1798e1787d272c3fe9bc5dcc71d0
Julio Dantas <juliodantas19@gmail.com>
2023-04-18 09:03:08 -0300

    PROJETO-99999 de aprendizado com spring

---
+++ src/main/java/com/julio/projetoaprendizado/ProjetoaprendizadoApplication.java
@@ -0,0 +1,13 @@
+package com.julio.projetoaprendizado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoaprendizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoaprendizadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julio/projetoaprendizado/ProjetoaprendizadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio.projetoaprendizado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoaprendizadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
