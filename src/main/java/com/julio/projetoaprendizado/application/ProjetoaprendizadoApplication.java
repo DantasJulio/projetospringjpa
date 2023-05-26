8644aea21b59292e125fb8838ede4090e32a858b
Julio Dantas <juliodantas19@gmail.com>
2023-05-26 14:40:42 -0300

    PROJETO-99999 Adicionado o persistence.xml | Adicionei as conexões de banco para o MySQL | Adicionei as notações de persistência na classe Categoria.

--- src/main/java/com/julio/projetoaprendizado/application/ProjetoaprendizadoApplication.java
+++ src/main/java/com/julio/projetoaprendizado/application/ProjetoaprendizadoApplication.java
@@ -1,4 +1,4 @@
-package com.julio.projetoaprendizado;
+package com.julio.projetoaprendizado.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/julio/projetoaprendizado/domain/Categoria.java
+++ src/main/java/com/julio/projetoaprendizado/domain/Categoria.java
@@ -3,13 +3,21 @@ package com.julio.projetoaprendizado.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "Nome", nullable = false, length = 50)
 	private String nome;
 	
 	public Categoria () {
