ccf884eab420c629fcc7806476902c36514bcfe8
Julio Dantas <juliodantas19@gmail.com>
2023-05-26 15:25:41 -0300

    PROJETO-99999 | Criando o Services e o Repository para a Categoria | Adicionando o método buscar pelo Id no Controller.

---
+++ src/main/java/com/julio/projetoaprendizado/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.julio.projetoaprendizado.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.julio.projetoaprendizado.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+
+}
--- src/main/java/com/julio/projetoaprendizado/resources/CategoriaResource.java
+++ src/main/java/com/julio/projetoaprendizado/resources/CategoriaResource.java
@@ -3,17 +3,23 @@ package com.julio.projetoaprendizado.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.julio.projetoaprendizado.domain.Categoria;
+import com.julio.projetoaprendizado.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping()
 	public List<Categoria> listar() {
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
@@ -23,7 +29,13 @@ public class CategoriaResource {
 		catList.add(cat2);
 		
 		return catList;
-		
+			
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> findCategory (Integer id ) {
+		Categoria categoria = service.buscarCategoria(id);
+		return ResponseEntity.ok().body(categoria);
 		
 	}
 	
