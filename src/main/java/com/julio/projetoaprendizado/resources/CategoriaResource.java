0e167727397fdd3b04025e8c36139220d021975b
Julio Dantas <juliodantas19@gmail.com>
2023-05-26 10:46:41 -0300

    PROJETO-99999 Adicionei a classe de domínio Categoria e seus atributos/métodos. Alterei o método da classe Resource para receber uma lista de Categorias e retornar o JSON

--- src/main/java/com/julio/projetoaprendizado/resources/CategoriaResource.java
+++ src/main/java/com/julio/projetoaprendizado/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.julio.projetoaprendizado.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.julio.projetoaprendizado.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> catList = new ArrayList<>();
+		catList.add(cat1);
+		catList.add(cat2);
+		
+		return catList;
+		
+		
 	}
 	
 }
